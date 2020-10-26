af0effd85ccb8fd22a9f4dc1a70499cca16bfa53
krathank <krathank@gmail.com>
2020-10-26 21:18:46 +0530

    VM and Network

---
+++ network-security-group/main.tf
@@ -0,0 +1,15 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.20.0"
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "demo" {
+  name     = "network-security-group-demo"
+  location = var.location
+  tags = {
+      env = "demo"
+  }
+}
